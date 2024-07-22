912f640286e85ed654dca2cf7cb315fe2da099e0
sajal-123 <work.sajalgarg.1914@gmail.com>
2024-07-22 23:17:15 +0530

    "Front_end Starting"

---
+++ front_end/app/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React, { FC, useState } from "react"
+
+interface Props { };
+
+
+const page: FC<Props> = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default page
---
+++ front_end/app/utils/Heading.tsx
@@ -0,0 +1,7 @@
+import React,{FC} from 'react'
+
+interface HeadProps{
+    title:string,
+    description:string,
+    keywords:string
+}
